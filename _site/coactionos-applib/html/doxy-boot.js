f511e4cb77129cb14f2004df75d959319496148b
tyler-gilbert <tgil@coactionos.com>
2013-12-29 21:02:54 -0700

    Integrated google sitemap

--- _site/coactionos-applib/html/doxy-boot.js
+++ _site/coactionos-applib/html/doxy-boot.js
@@ -19,7 +19,8 @@ $( document ).ready(function() {
 	$('img[src="ftv2cl.png"]').replaceWith('<span class="label label-danger">C</span> ');
 	
 	$("ul.tablist").addClass("nav nav-pills nav-justified");
-	$("ul.tablist").css("margin-top", "1em");
+	$("ul.tablist").css("margin-top", "0.5em");
+	$("ul.tablist").css("margin-bottom", "0.5em");
 	$("li.current").addClass("active");
 	$("iframe").attr("scrolling", "yes");
 	
